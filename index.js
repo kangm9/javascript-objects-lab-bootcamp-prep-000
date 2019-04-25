e237d0f2f994931acced097e5dfc3edfddfe2607
Min Kang <kangm914@gmail.com>
2019-04-25 15:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,22 +15,5 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  delete newObj;
 }
-  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
-    it('returns object without the delete key/value pair', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('modifies the original object', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(obj['prop']).toBe(undefined)
-    })
-  })
-
-})
