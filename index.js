9b6dc7d2a3b5e51df7e1c3356fdfc631beb787d4
Min Kang <kangm914@gmail.com>
2019-04-25 15:26:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,40 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+describe('deleteFromObjectByKey(object, key)', function() {
+    it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+      var newObj = deleteFromObjectByKey(obj, 'prop')
+
+      expect(newObj['prop']).toBe(undefined)
+    })
+
+    it('does not modify the original object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+
+      deleteFromObjectByKey(obj, 'prop')
+      expect(obj['prop']).toBe(1)
+    })
+  })
+
+  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
+    it('returns object without the delete key/value pair', function() {
+      var obj = { prop: 1 }
+      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+      expect(newObj['prop']).toBe(undefined)
+    })
+
+    it('modifies the original object', function() {
+      var obj = { prop: 1 }
+      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+      expect(obj['prop']).toBe(undefined)
+    })
+  })
+
+})
